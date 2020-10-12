a00fbd09f1fda497c502cfa8a1b7127e649cf613
sahanchamika <66235894+sahanchamika@users.noreply.github.com>
2020-10-12 15:23:10 +0530

    Update conection.php

--- serprise/conection.php
+++ serprise/conection.php
@@ -1,7 +1,7 @@
 <?php
 $con = mysqli_connect("localhost","root","","serpriselk");
 
-if (mysqli_connect_errno()) {
+if (mysqli_connect_error()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   exit();
 }
